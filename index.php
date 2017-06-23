47fd08ac33534875fbcb329c4b4bb596e9922ba1
= <=>
2017-06-23 12:15:00 +0530

    updated gitignore file added development configs jbimages plugin path changed

--- index.php
+++ index.php
@@ -53,7 +53,24 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+
+ // change the UAT sever address when we get one sanctioned
+if($_SERVER['SERVER_NAME'] == 'bimpra.tk'){
+	define('ENVIRONMENT', 'production');
+}else{
+	define('ENVIRONMENT', 'development');
+}
+
+function __autoload($classname) {
+
+	if (strpos($classname, 'CI_') !== 0) 	{
+		$file = APPPATH . 'libraries/' . $classname . '.php';
+		if (file_exists($file) && is_file($file)) {
+			@include_once($file);
+		}
+	}
+
+}
 
 /*
  *---------------------------------------------------------------
@@ -66,29 +83,29 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
+	error_reporting(-1);
+	ini_set('display_errors', 1);
 	break;
 
 	case 'testing':
 	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
+	ini_set('display_errors', 0);
+	if (version_compare(PHP_VERSION, '5.3', '>='))
+	{
+		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+	}
+	else
+	{
+		error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+	}
 	break;
 
 	default:
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
+	header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+	echo 'The application environment is not set correctly.';
 		exit(1); // EXIT_ERROR
-}
-error_reporting(E_ALL);
+	}
+	error_reporting(E_ALL);
 /*
  *---------------------------------------------------------------
  * SYSTEM FOLDER NAME
@@ -98,7 +115,7 @@ error_reporting(E_ALL);
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = 'system';
+$system_path = 'system';
 
 /*
  *---------------------------------------------------------------
@@ -113,7 +130,7 @@ error_reporting(E_ALL);
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'application';
+$application_folder = 'application';
 
 /*
  *---------------------------------------------------------------
@@ -128,7 +145,7 @@ error_reporting(E_ALL);
  *
  * NO TRAILING SLASH!
  */
-	$view_folder = '';
+$view_folder = '';
 
 
 /*
@@ -190,26 +207,26 @@ error_reporting(E_ALL);
  */
 
 	// Set the current directory correctly for CLI requests
-	if (defined('STDIN'))
-	{
-		chdir(dirname(__FILE__));
-	}
+if (defined('STDIN'))
+{
+	chdir(dirname(__FILE__));
+}
 
-	if (($_temp = realpath($system_path)) !== FALSE)
-	{
-		$system_path = $_temp.'/';
-	}
-	else
-	{
+if (($_temp = realpath($system_path)) !== FALSE)
+{
+	$system_path = $_temp.'/';
+}
+else
+{
 		// Ensure there's a trailing slash
-		$system_path = rtrim($system_path, '/').'/';
-	}
+	$system_path = rtrim($system_path, '/').'/';
+}
 
 	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
+if ( ! is_dir($system_path))
+{
+	header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+	echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
 		exit(3); // EXIT_CONFIG
 	}
 
@@ -219,33 +236,33 @@ error_reporting(E_ALL);
  * -------------------------------------------------------------------
  */
 	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
 	// Path to the system folder
-	define('BASEPATH', str_replace('\\', '/', $system_path));
+define('BASEPATH', str_replace('\\', '/', $system_path));
 
 	// Path to the front controller (this file)
-	define('FCPATH', dirname(__FILE__).'/');
+define('FCPATH', dirname(__FILE__).'/');
 
 	// Name of the "system folder"
-	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
 
 	// The path to the "application" folder
-	if (is_dir($application_folder))
+if (is_dir($application_folder))
+{
+	if (($_temp = realpath($application_folder)) !== FALSE)
 	{
-		if (($_temp = realpath($application_folder)) !== FALSE)
-		{
-			$application_folder = $_temp;
-		}
-
-		define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
+		$application_folder = $_temp;
 	}
-	else
+
+	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
+}
+else
+{
+	if ( ! is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
 	{
-		if ( ! is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
-		{
-			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-			echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
 			exit(3); // EXIT_CONFIG
 		}
 
